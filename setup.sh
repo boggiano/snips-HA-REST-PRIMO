4ae626b5e12ae4884f5fff7ea6cc5aa6443dd259
boggiano <boggiano@gmail.com>
2019-04-15 00:41:19 +0200

    cambio to python3

--- setup.sh
+++ setup.sh
@@ -1,20 +1,29 @@
-#/usr/bin/env bash -e
+#!/usr/bin/env bash
+set -e
 
+PYTHON=$(command -v python3)
 VENV=venv
 
-if [ ! -d "$VENV" ]
-then
+if [ -f "$PYTHON" ]; then
 
-    PYTHON=`which python2`
-
-    if [ ! -f $PYTHON ]
-    then
-        echo "could not find python"
+    if [ ! -d $VENV ]; then
+        # Create a virtual environment if it doesn't exist.
+        $PYTHON -m venv $VENV
+    else
+        if [ -e $VENV/bin/python2 ]; then
+            # If a Python2 environment exists, delete it first
+            # before creating a new Python 3 virtual environment.
+            rm -r $VENV
+            $PYTHON -m venv $VENV
+        fi
     fi
-    virtualenv -p $PYTHON $VENV
 
+    # Activate the virtual environment and install requirements.
+    # shellcheck disable=SC1090
+    . $VENV/bin/activate
+    pip3 install wheel
+    pip3 install -r requirements.txt
+
+else
+    >&2 echo "Cannot find Python 3. Please install it."
 fi
-
-. $VENV/bin/activate
-
-pip install -r requirements.txt
